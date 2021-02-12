2ccaecfe9b8c70bfc9a40061f98eb3df6f52fdf4
user <user@NB-GIAC>
2021-02-12 16:51:04 +0100

    sda

---
+++ AgenziaEntrate/src/main/java/advprogproj/AgenziaEntrate/utils/StringToLocalDateConverter.java
@@ -0,0 +1,28 @@
+package advprogproj.AgenziaEntrate.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringToLocalDateConverter implements Converter<String, LocalDate> {
+
+	@Override
+	public LocalDate convert(String source) {
+		LocalDate res = null;
+		
+		try {
+			res = LocalDate.parse(source, DateTimeFormatter.ISO_LOCAL_DATE);
+		} catch (DateTimeParseException e) {
+			try {
+				res = LocalDate.parse(source, DateTimeFormatter.ofPattern("dd/MM/yy"));
+			} catch (DateTimeParseException e1) {
+				// nothing to do
+			}
+		}	
+		return res;
+	}	
+}
